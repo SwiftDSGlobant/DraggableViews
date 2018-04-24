d56501cebf8119ccf60f36563bbb3de5b91cf49b
Aldo Antonio Martinez Avalos <aldo.martinez@globant.com>
2018-04-24 00:21:35 -0500

    Initial Commit

---
+++ DragView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragView
+//
+//  Created by Aldo Antonio Martinez Avalos on 24/04/18.
+//  Copyright © 2018 Aldo Antonio Martinez Avalos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
